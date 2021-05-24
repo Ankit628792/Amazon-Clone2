bf60cbad10660cbb3b8af5d6cb0f7bd124d62579
Ankit <ankit628792@gmail.com>
2021-05-24 17:32:28 +0530

    Update checkout.js

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -7,7 +7,7 @@ import Currency from 'react-currency-formatter'
 import { useSession } from 'next-auth/client'
 import { loadStripe } from '@stripe/stripe-js'
 import axios from 'axios'
-const stripePromise = loadStripe(process.env.stripe_public_key)
+const stripePromise = await loadStripe(process.env.stripe_public_key)
 
 function Checkout() {
     const items = useSelector(selectItems)
