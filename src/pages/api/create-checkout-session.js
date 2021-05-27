1b7b98fa184af2de1c625ef921c3d12ebb705c50
Ankit <ankit628792@gmail.com>
2021-05-27 22:14:41 +0530

    Update create-checkout-session.js

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,22 +1,24 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 export default async (req, res) => {
+ 
     const { items, email } = req.body;
-    const transformedItems = items.map((item) => ({
 
-        description: item.product.description,
-        quantity: 1,
-        price_data: {
-            currency: 'inr',
-            unit_amount: item.product.price * 1000,
-            product_data: {
-                name: item.product.title,
-                images: [item.product.image]
-            }
+  const transformedItems = items.map((item) => ({
+    description: item.product.description,
+    quantity: 1,
+    price_data: {
+        currency: 'inr',
+        unit_amount: item.product.price * 1000,
+        product_data: {
+            name: item.product.title,
+            images: [item.product.image]
         }
-    }))
+    }
+}))
 
-
+  try {
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         shipping_rates: ['shr_1Iu5fASHt26EdXYY99cHlrM6'],
@@ -32,9 +34,13 @@ export default async (req, res) => {
             images: JSON.stringify(items.map((item) => item.product.image))
         }
     })
+    
+    return res.status(200).json({ id: session.id });
 
-    res.status(200).json({ id: session.id })
-}
-
-// stripe listen --forward-to localhost:3000/api/webhook
-
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({
+      error: { message: e.message },
+    });
+  }
+};
